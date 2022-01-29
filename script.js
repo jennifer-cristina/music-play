6ce1429821751463f458f1d55b8d988af528eccc
Jennifer Cristina <cristin4.jennifer@gmail.com>
2022-01-29 01:51:25 -0300

    Construido um reprodutor de musica com base em duas aulas de joão tinti

---
+++ script.js
@@ -0,0 +1,83 @@
+let musicas = [
+    {titulo:'Blue Dream', artista:'Patrick Patrikios', src:'msc/Blue Dream - Cheel.mp3', img:'img/clouds.jpg'},
+    {titulo:'Drown Me Out', artista:'VYEN', src:'msc/Drown Me Out - VYEN.mp3', img:'img/art.jpg'},
+    {titulo:'Sunset Dream', artista:'Cheel', src:'msc/Sunset Dream - Cheel.mp3', img:'img/forest.jpg'}
+];
+
+// Buscando cada classe do html
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    // validação na aplicação para que nao encontre um index vazio (undefinned)
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proximo').addEventListener('click', () => {
+    indexMusica++;
+    // validação na aplicação para que nao encontre um index vazio (undefinned)
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+// Funções
+function renderizarMusica(index){
+    // estou substituindo o atributo src original da minha tag audio pelo atributo src que esta no array musicas
+    musica.setAttribute('src', musicas[index].src);
+    // após o usuário realizar o click de proxima ou anterior estará buscando os dados do array conforme seu index
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos + ':' +campoSegundos;
+}
